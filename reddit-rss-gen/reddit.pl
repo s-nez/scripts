ae0f5f26e5dc0ad73819ad49cbc6f85544e44d96
Szymon Nieznański <s.nez@member.fsf.org>
2015-01-22 21:04:45 +0100

    Got rid of the username file

--- reddit-rss-gen/reddit.pl
+++ reddit-rss-gen/reddit.pl
@@ -8,10 +8,27 @@ use Reddit::Client;
 # Required to output UTF-8 characters correctly
 binmode STDOUT, ":utf8";
 
-my $config_dir = $ENV{HOME} . "/.reddit";
+my $login;
+my $subreddit;
+
+sub parse_args {
+	while (@ARGV) {
+		my $opt = shift @ARGV;
+		if ($opt =~ /\A(?:-u)|(?:--user)\Z/) {
+			$login = shift @ARGV;
+		} elsif ($opt =~ /\A(?:-s)|(?:--subreddit)\Z/) {
+			$subreddit = '/r/' . shift @ARGV;
+		} else {
+			die "Unrecognised option: $opt";
+		}
+	}
+}
+
+parse_args();
+
+my $config_dir = $ENV{HOME} . '/.reddit/' . $login;
 my $session_file = $config_dir . '/session';
 my $user_hash_file = $config_dir . '/hash';
-my $username_file = $config_dir . '/user';
 
 mkdir $config_dir unless (-e $config_dir and -d $config_dir);
 
@@ -22,14 +39,10 @@ my $reddit       = Reddit::Client->new(
 
 unless ($reddit->is_logged_in) {
 	print "Login: ";
-	chomp (my $login = <>);
+	chomp ($login = <>) unless length $login;
 	my $password = prompt('Password: ', -e => '*');
 	$reddit->login($login, $password);
 	$reddit->save_session();
-
-	open my $UF, '>', $username_file;
-	print $UF $login;
-	close $UF;
 }
 
 unless (-e $user_hash_file) {
@@ -44,11 +57,7 @@ open my $UHF, '<', $user_hash_file or die $!;
 my $user_hash = <$UHF>;
 close $UHF;
 
-open my $UF, '<', $username_file or die $!;
-my $username = <$UF>;
-close $UF;
-
 my $links = $reddit->fetch_links(subreddit => '/r/slimak');
 foreach (@{$links->{items}}) {
-	say $_->{url} . '.rss?feed=' . $user_hash . '&user=' . $username;
+	say $_->{url} . '.rss?feed=' . $user_hash . '&user=' . $login;
 }
