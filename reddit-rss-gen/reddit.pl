cc59ffa4b8c5270c6b9844eab4329bc6fced8fc0
Szymon Nieznański <s.nez@member.fsf.org>
2015-01-22 17:49:11 +0100

    Added the Reddit RSS generator

---
+++ reddit-rss-gen/reddit.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature 'say';
+use IO::Prompt;
+use Reddit::Client;
+use POSIX 'cuserid';
+
+# Required to output UTF-8 characters correctly
+binmode STDOUT, ":utf8";
+
+my $user = POSIX::cuserid();
+my $config_dir = "/home/$user/.reddit";
+my $session_file = $config_dir . '/session';
+my $user_hash_file = $config_dir . '/hash';
+my $username_file = $config_dir . '/user';
+
+mkdir $config_dir unless (-e $config_dir and -d $config_dir);
+
+my $reddit       = Reddit::Client->new(
+	session_file => $session_file,
+	user_agent   => 'Hue/1.0',
+);
+
+unless ($reddit->is_logged_in) {
+	print "Login: ";
+	chomp (my $login = <>);
+	my $password = prompt('Password: ', -e => '*');
+	$reddit->login($login, $password);
+	$reddit->save_session();
+
+	open my $UF, '>', $username_file;
+	print $UF $login;
+	close $UF;
+}
+
+unless (-e $user_hash_file) {
+	print 'User authorisation hash: ';
+	chomp (my $hash = <>);
+	open my $UHF, '>', $user_hash_file or die $!;
+	print $UHF $hash;
+	close $UHF;
+}
+
+open my $UHF, '<', $user_hash_file or die $!;
+my $user_hash = <$UHF>;
+close $UHF;
+
+open my $UF, '<', $username_file or die $!;
+my $username = <$UF>;
+close $UF;
+
+my $links = $reddit->fetch_links(subreddit => '/r/slimak');
+foreach (@{$links->{items}}) {
+	say $_->{url} . '.rss?feed=' . $user_hash . '&user=' . $username;
+}
