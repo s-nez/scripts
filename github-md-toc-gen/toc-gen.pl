90050a1c8ec90cce99db5095abcac84429f83ef5
Szymon Nieznański <s.nez@member.fsf.org>
2015-03-31 01:54:17 +0200

    Add a switch to surround the ToC with delimeters

--- github-md-toc-gen/toc-gen.pl
+++ github-md-toc-gen/toc-gen.pl
@@ -2,15 +2,25 @@
 use strict;
 use warnings;
 use feature 'say';
+use Getopt::Std;
+
 binmode STDOUT, 'utf8';
 
 my $tab        = ' ' x 4;
 my $start_hlvl = 1;
 
+my ($start_delim, $end_delim) = ('<!--TOC_START--->', '<!--TOC_END-->');
+
+# Commandline options 
+our $opt_d = 0; # add delimeters to the ToC
+our $opt_w = 0; # write the ToC into the source file (TODO)
+getopts('dw');
+
 @ARGV or die 'You need to specify a filename';
 my ($filename) = @ARGV;
 open my $FH, '<:utf8', $filename or die $!;
 
+say $start_delim if $opt_d;
 while (<$FH>) {
     if (/\A#{$start_hlvl}(#+) (.+)\Z/) {
         chomp;
@@ -22,5 +32,5 @@ while (<$FH>) {
         say $tab x $lvl, "* [$title](#$link)";
     }
 }
-
+say $end_delim if $opt_d;
 close $FH;
