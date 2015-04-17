79dd2a075447960c96757c05ae18bb23c9f4a1b6
Szymon Nieznański <s.nez@member.fsf.org>
2015-04-17 20:29:24 +0200

    Use a dispatch table to parse input and ignore existing ToC

--- github-md-toc-gen/toc-gen.pl
+++ github-md-toc-gen/toc-gen.pl
@@ -8,53 +8,76 @@ use Getopt::Std;
 binmode STDOUT, 'utf8';
 
 my $tab = ' ' x 4;
-my ($min_hlvl, $max_hlvl) = (1, 4); # TODO: Make this configurable
+my ($min_hlvl, $max_hlvl) = (1, 4);    # TODO: Make this configurable
 
 my ($start_delim, $end_delim) = ('<!--TOC_START--->', '<!--TOC_END--->');
 
 # Commandline options
-our $opt_d = 0;     # add delimeters to the ToC
-our $opt_w = 0;     # write the ToC into the source file
-our $opt_t = '';    # add a custom title for the ToC
+our $opt_d = 0;                        # add delimeters to the ToC
+our $opt_w = 0;                        # write the ToC into the source file
+our $opt_t = '';                       # add a custom title for the ToC
 getopt('t');
 getopts('dw');
 
+sub toc_add(\@$$) {
+    my ($toc, $raw_level, $title) = @_;
+    return if length($raw_level) + $min_hlvl > $max_hlvl;
+
+    my $lvl = length($raw_level) - $min_hlvl;
+    $title =~ s/\s+$//;                # remove trailing whitespace
+    my $link = lc $title;
+    $link =~ s/\s+/-/g;
+    $link =~ s/[^\w-]//gu;
+    push @$toc, $tab x $lvl . "* [$title](#$link)";
+}
+
 @ARGV or die 'You need to specify a filename';
 my ($filename) = @ARGV;
-open my $FH, '<:utf8', $filename or die $!;
+open my $SOURCE, '<:utf8', $filename or die $!;
 
 my @toc;
 push @toc, $start_delim if $opt_d;
 push @toc, '#' x ($min_hlvl + 1) . ' ' . decode_utf8($opt_t) if $opt_t;
-while (<$FH>) {
-    if (/\A#{$min_hlvl}(#+) (.+)\Z/) {
-        next if length($1) + $min_hlvl > $max_hlvl;
-        chomp;
-        my $lvl   = length($1) - $min_hlvl;
-        my $title = $2;
-        my $link  = lc $title;
-        $link =~ s/\s+/-/g;
-        $link =~ s/[^\w-]//gu;
-        push @toc, $tab x $lvl . "* [$title](#$link)";
+
+my $toc_status  = 'before';    # possible values: before, inside, after
+my %parse_input = (
+    before => sub {
+        my $line = shift;
+        if ($line =~ /$start_delim/) {
+            $toc_status = 'inside';
+        } elsif ($line =~ /\A#{$min_hlvl}(#+) (.+)\Z/) {
+            toc_add(@toc, $1, $2);
+        }
+    },
+    inside => sub {
+        my $line = shift;
+        $toc_status = 'after' if ($line =~ /$end_delim/);
+    },
+    after => sub {
+        my $line = shift;
+        if ($line =~ /\A#{$min_hlvl}(#+) (.+)\Z/) {
+            toc_add(@toc, $1, $2);
+        }
     }
-}
+);
+$parse_input{$toc_status}->($_) while (<$SOURCE>);
 
 # newline required for Markdown to treat this as a comment
 push @toc, "\n" . $end_delim if $opt_d;
 
 unless ($opt_w) {
-    close $FH;
+    close $SOURCE;
     say foreach @toc;
     exit 0;
 }
 
 # Option -w selected
-my $toc_status = 'before';             # possible values: before, inside, after
 my $tmp_file   = "/tmp/ghtocgen-$$";
 open my $TMP, '>:utf8', $tmp_file or die $!;
-seek $FH, 0, 0;
+seek $SOURCE, 0, 0;
 
-while (<$FH>) {
+$toc_status = 'before';             # possible values: before, inside, after
+while (<$SOURCE>) {
     if ($toc_status eq 'before') {
         if (index($_, $start_delim) != -1) {
             say $TMP $_ foreach @toc;
@@ -71,7 +94,7 @@ while (<$FH>) {
     }
 }
 
-close $FH;
+close $SOURCE;
 close $TMP;
 
 if ($toc_status eq 'before') {
